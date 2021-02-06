eefb1ccb610f4e4a69637f0f1be05ade943a9075
nohreit <raelfrost07@gmail.com>
2021-02-06 15:40:53 -0500

    update

---
+++ lab/index.js
@@ -0,0 +1,7 @@
+function on() {
+    document.getElementsByClassName("overlay").style.display = "block";
+  }
+  
+  function off() {
+    document.getElementsByClassName("overlay").style.display = "none";
+  }
